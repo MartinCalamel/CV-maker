d0765775bff299d62f333e894f93213acf7361dd
MartinCalamel <martin.calamel@cpe.fr>
2024-11-07 21:38:37 +0100

    big update profile

--- code/profile.py
+++ code/profile.py
@@ -2,19 +2,20 @@ from data import Data
 
 class Profile : 
     def __init__(self, profile_name : str):
-        self.file_profile = "profile.json"
+        self.file_profile = "code/profile_test.json"
         self.profile_elements = [["surname",""],
                                  ["name",""],
                                  ["address",""],
                                  ["phone",""],
                                  ["email",""],
                                  ["github",""],
-                                 ["experience",["title","company","position","dates"]],
-                                 ["education",["institution","dates"]],
+                                 ["experience",[["title",""],["company",""],["position",""],["dates",""]]],
+                                 ["education",[["institution",""],["dates",""]]],
                                  ["awards",[]],
                                  ["skills",[]],
                                  ["hobbies",[]]]
         self.data=Data(self.file_profile)
+        self.profile = self.set_profile(profile_name)
     
     def set_profile(self, profile_name : str) -> dict:
         """
@@ -34,24 +35,37 @@ class Profile :
         self.data.add_category(profile_name,data)
         return data
     
-    def make_profile(self,profile_elements : list):
+    def make_profile(self,profile_elements : list) -> dict:
         data={}
         for element in profile_elements:
             if element[1] == "" :
                 data[element[0]] = input(f'{element[0]} : ')
-            elif element[1] == []:
-                data[element[0]] = self.add_list_element(element[0])
             elif type(element[1]) == list:
-                for i in element[1]:
-                    self.make_profile()
+                data[element[0]] = self.add_list_element(element)
+        
+        return data
     
-    def add_list_element(self, element_name : str) -> list:
+    def add_list_element(self, element : list) -> list:
         res = []
         stop = False
-        while not stop:
-            new = input(f'element to add to {element_name} (q for stop) : ')
-            if new == "q":
-                stop = True
-            else:
-                res.append(new)
-        return new
+
+        if element[1] == []:
+            while not stop:
+                new = input(f'element to add to {element[0]} (q for stop) : ')
+                if new == "q":
+                    stop = True
+                else:
+                    res.append(new)
+
+        else :
+            while not stop:
+                stop = input(f'add new element to {element[0]} (q for stop other to continue) : ')=='q'
+                if not stop :
+                    new= self.make_profile(element[1])
+                    res.append(new)
+        return res
+
+
+if __name__ == '__main__' :
+    profile=Profile("profile1")
+    print(profile.data)
